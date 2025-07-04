d99a10e26857da280a26a0fcc69ac39b2fe72e65
alice20240103 <alice20240103@gamil.com>
2025-07-04 16:02:00 +0900

    first commit

---
+++ src/main/java/com/example/demo/Kosta0704Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kosta0704Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kosta0704Application.class, args);
+	}
+
+}
